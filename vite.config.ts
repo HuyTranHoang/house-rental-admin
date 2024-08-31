37db6a8e7ef35cfbfb002e1460a566806cb281b1
Huy Trần <nhokpio95@gmail.com>
2024-08-31 13:54:15 +0700

    Cập nhật cross evn cho vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { dirname } from 'path'
 import { fileURLToPath } from 'node:url'
-import path from 'path'
+import path, { dirname } from 'path'
+import { defineConfig } from 'vite'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+
+const apiTarget = process.env.API_TARGET || 'http://localhost:8080/api'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,9 +20,7 @@ export default defineConfig({
     port: 3100,
     proxy: {
       '/api': {
-        // Bỏ comment dòng dưới để chạy ở local
-        // target: 'http://localhost:8080/api',
-        target: 'https://house-rental-latest.onrender.com/api',
+        target: apiTarget,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
