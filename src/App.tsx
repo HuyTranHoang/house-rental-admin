bc362636f8a97132f1b22bb9b3d65e36dda4e0d8
Huy Trần <nhokpio95@gmail.com>
2024-09-01 23:01:47 +0700

    Add useRefreshToken

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
 import CustomIndicator from '@/components/CustomIndicator.tsx'
+import useRefreshToken from '@/hooks/useRefreshToken.ts'
+import router from '@/router/router.tsx'
 import { Spin } from 'antd'
-import { useEffect, useState } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import './App.css'
-import axiosInstance from './axiosInstance.ts'
-import { loginFailure, loginSuccess } from './features/auth/authSlice.ts'
-import router from './router/router.tsx'
-import { useAppDispatch } from './store.ts'
-import { User } from '@/models/user.type.ts'
 
 function App() {
-  const dispatch = useAppDispatch()
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    const refresh = async () => {
-      try {
-        setIsLoading(true)
-        const response = await axiosInstance.post<User>('/api/auth/refresh-token', {}, { withCredentials: true })
-        const user = response.data
-        const newJwtToken = response.headers['jwt-token']
-
-        localStorage.setItem('jwtToken', newJwtToken)
-        dispatch(loginSuccess({ user, token: newJwtToken }))
-      } catch (error) {
-        console.error('Refresh token failed:', error)
-        dispatch(loginFailure())
-        localStorage.removeItem('jwtToken')
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
-    refresh().then(() => console.log('>>>APP.JSX', 'App is mounted'))
-  }, [dispatch])
+  const isLoading = useRefreshToken()
 
   if (isLoading) {
     return <Spin indicator={<CustomIndicator />} tip={'Đang tải dữ liệu...Vui lòng đợi trong giây lát!!!'} fullscreen />
---
+++ src/hooks/useRefreshToken.ts
@@ -0,0 +1,36 @@
+import { loginFailure, loginSuccess } from '@/features/auth/authSlice.ts'
+import { User } from '@/models/user.type.ts'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import axiosInstance from '@/axiosInstance.ts'
+
+const useRefreshToken = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const refresh = async () => {
+      try {
+        setIsLoading(true)
+        const response = await axiosInstance.post<User>('/api/auth/refresh-token', {}, { withCredentials: true })
+        const user = response.data
+        const newJwtToken = response.headers['jwt-token']
+
+        localStorage.setItem('jwtToken', newJwtToken)
+        dispatch(loginSuccess({ user, token: newJwtToken }))
+      } catch (error) {
+        console.error('Refresh token failed:', error)
+        dispatch(loginFailure())
+        localStorage.removeItem('jwtToken')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    refresh()
+  }, [dispatch])
+
+  return isLoading
+}
+
+export default useRefreshToken
