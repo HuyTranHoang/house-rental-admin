d02cb477becca085d8e1bbff03f1ed0ff4aacd4d
Huy Trần <nhokpio95@gmail.com>
2024-09-19 00:29:18 +0700

    Thêm giá trị theme và i18n vào localStorage

--- src/utils/i18n/i18n.ts
+++ src/utils/i18n/i18n.ts
@@ -6,23 +6,23 @@ import en_amanity from '@/utils/i18n/locales/en/amenity.json'
 import en_breadcrumbs from '@/utils/i18n/locales/en/breadcrumbs.json'
 import en_city from '@/utils/i18n/locales/en/city.json'
 import en_common from '@/utils/i18n/locales/en/common.json'
-import en_review from '@/utils/i18n/locales/en/review.json'
-import en_roomType from '@/utils/i18n/locales/en/roomType.json'
-import en_report from '@/utils/i18n/locales/en/report.json'
-import en_user from '@/utils/i18n/locales/en/user.json'
 import en_district from '@/utils/i18n/locales/en/district.json'
+import en_report from '@/utils/i18n/locales/en/report.json'
+import en_review from '@/utils/i18n/locales/en/review.json'
 import en_role from '@/utils/i18n/locales/en/role.json'
+import en_roomType from '@/utils/i18n/locales/en/roomType.json'
+import en_user from '@/utils/i18n/locales/en/user.json'
 
 import vi_amanity from '@/utils/i18n/locales/vi/amenity.json'
 import vi_breadcrumbs from '@/utils/i18n/locales/vi/breadcrumbs.json'
 import vi_city from '@/utils/i18n/locales/vi/city.json'
 import vi_common from '@/utils/i18n/locales/vi/common.json'
-import vi_review from '@/utils/i18n/locales/vi/review.json'
-import vi_roomType from '@/utils/i18n/locales/vi/roomType.json'
-import vi_report from '@/utils/i18n/locales/vi/report.json'
-import vi_user from '@/utils/i18n/locales/vi/user.json'
 import vi_district from '@/utils/i18n/locales/vi/district.json'
+import vi_report from '@/utils/i18n/locales/vi/report.json'
+import vi_review from '@/utils/i18n/locales/vi/review.json'
 import vi_role from '@/utils/i18n/locales/vi/role.json'
+import vi_roomType from '@/utils/i18n/locales/vi/roomType.json'
+import vi_user from '@/utils/i18n/locales/vi/user.json'
 
 export const defaultNS = 'common'
 export const resources = {
@@ -35,8 +35,8 @@ export const resources = {
     roomType: en_roomType,
     report: en_report,
     user: en_user,
-    district : en_district ,
-    role : en_role,
+    district: en_district,
+    role: en_role
   },
   vi: {
     common: vi_common,
@@ -47,8 +47,8 @@ export const resources = {
     roomType: vi_roomType,
     report: vi_report,
     user: vi_user,
-    district : vi_district,
-    role : vi_role,
+    district: vi_district,
+    role: vi_role
   }
 } as const
 
@@ -58,9 +58,9 @@ i18n
   .init({
     resources,
     defaultNS,
-    lng: 'en',
+    lng: localStorage.getItem('i18n') || 'en',
     fallbackLng: ['en', 'vi'],
-    ns: ['common', 'breadcrumbs', 'city', 'review', 'amenity', 'roomType', 'report', 'user','district','role'],
+    ns: ['common', 'breadcrumbs', 'city', 'review', 'amenity', 'roomType', 'report', 'user', 'district', 'role'],
     interpolation: {
       escapeValue: false
     }
