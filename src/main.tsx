0b66c36574c00da1d6af4926595ed35406d3f9a3
Huy Trần <nhokpio95@gmail.com>
2024-09-18 19:15:19 +0700

    Fix error redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,21 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+
+import i18n from '@/utils/i18n/i18n.ts'
+import { I18nextProvider } from 'react-i18next'
 import App from './App.tsx'
 import './index.css'
-import store from './store.ts'
-import { I18nextProvider } from 'react-i18next'
-import i18n from '@/utils/i18n/i18n.ts'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
