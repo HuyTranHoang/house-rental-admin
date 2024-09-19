9542735a38e4ce86433f6f04eb6157e9f53cc89c
Huy Trần <nhokpio95@gmail.com>
2024-09-20 05:54:57 +0700

    Sửa lỗi loading lúc f5

--- src/hooks/useRefreshToken.ts
+++ src/hooks/useRefreshToken.ts
@@ -1,22 +1,25 @@
-import axiosInstance from '@/axiosInstance.ts'
+import useAuthStore from '@/features/auth/authStore.ts'
+import axiosInstance from '@/inteceptor/axiosInstance.ts'
 import { User } from '@/models/user.type.ts'
-import useBoundStore from '@/store.ts'
+import { delay } from '@/utils/delay.ts'
 import { useEffect, useState } from 'react'
 
 const useRefreshToken = () => {
   const [isLoading, setIsLoading] = useState(true)
 
   const accessToken = localStorage.getItem('jwtToken')
-  const loginSuccess = useBoundStore((state) => state.loginSuccess)
+  const loginSuccess = useAuthStore((state) => state.loginSuccess)
 
   useEffect(() => {
     const refresh = async () => {
       try {
         if (!accessToken) {
+          console.log('No access token')
+          await delay(500)
+          setIsLoading(false)
           return
         }
 
-        setIsLoading(true)
         const response = await axiosInstance.post<User>('/api/auth/refresh-token', {}, { withCredentials: true })
         const user = response.data
         const newJwtToken = response.headers['jwt-token']
